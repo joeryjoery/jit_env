aae10413b6a022d1867992881acfabf978ac4ff0
Joery de Vries <joery-de-vries@hotmail.com>
2023-04-11 09:42:17 +0200

    bump version to 0.1.0

--- jit_env/version.py
+++ jit_env/version.py
@@ -5,5 +5,5 @@ def _version_as_tuple(version_str: str) -> tuple[int, ...]:
     return tuple(int(i) for i in version_str.split(".") if i.isdigit())
 
 
-__version__: str = '0.0.1alpha'
+__version__: str = '0.1.0'
 __version_info__: tuple[int, ...] = _version_as_tuple(__version__)
